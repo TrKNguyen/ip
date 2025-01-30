8dd69a4a52a2737f39c7dd50e6691a86c99b0a6e
TrKNguyen <nguyenusaco@gmail.com>
2025-01-30 19:35:19 +0800

    add: ingore .class file and change name of chatbot

---
+++ src/main/java/TristanNguyen.java
@@ -0,0 +1,6 @@
+public class TristanNguyen {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm TristanNguyen");
+        System.out.println("What can I do for you ");
+    }
+}
