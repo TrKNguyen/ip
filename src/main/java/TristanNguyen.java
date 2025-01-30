04152ef4f2b4f267f88ed2570816d662d9593b04
TrKNguyen <nguyenusaco@gmail.com>
2025-01-30 20:31:54 +0800

    Add Level 1. Echo

--- src/main/java/TristanNguyen.java
+++ src/main/java/TristanNguyen.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class TristanNguyen {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm TristanNguyen");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        while (true) {
+            String item = scanner.nextLine();
+            if (item.equals("bye")) {
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(item);
+                System.out.println("____________________________________________________________");
+            }
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
