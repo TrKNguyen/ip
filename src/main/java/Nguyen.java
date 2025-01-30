81684bdcd3e570fca45bb7b335380c0867d220ff
TrKNguyen <nguyenusaco@gmail.com>
2025-01-31 01:57:01 +0800

    Adding Level 6. Delete

--- src/main/java/Nguyen.java
+++ src/main/java/Nguyen.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 public class Nguyen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] listTask = new Task[101];
-        int size = 0;
+        ArrayList<Task> listTask = new ArrayList<Task>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Nguyen");
         System.out.println("What can I do for you?");
@@ -17,33 +18,42 @@ public class Nguyen {
                 System.out.println("____________________________________________________________");
                 if (item.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 1; i <= size; i++) {
-                        System.out.println(i + "." + listTask[i]);
+                    for (int i = 1; i <= listTask.size(); i++) {
+                        System.out.println(i + "." + listTask.get(i - 1));
                     }
-                } else if (item.startsWith("mark")) {
-                    int number = Integer.parseInt(item.substring(5));
-                    if (number > size || number <= 0) {
+                } else if (item.startsWith("delete")) {
+                    int number = Integer.parseInt(item.substring(7));
+                    if (number > listTask.size() || number <= 0) {
                         throw new NguyenException("Out of range in list of task");
                     }
-                    listTask[number].mark();
+                    System.out.println("Got it. I've removed this task:");
+                    System.out.println(listTask.get(number - 1));
+                    listTask.remove(number - 1);
+                    System.out.println("Now you have " + listTask.size() + " tasks in the list.");
+                }
+                else if (item.startsWith("mark")) {
+                    int number = Integer.parseInt(item.substring(5));
+                    if (number > listTask.size() || number <= 0) {
+                        throw new NguyenException("Out of range in list of task");
+                    }
+                    listTask.get(number - 1).mark();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(listTask[number]);
+                    System.out.println(listTask.get(number - 1));
                 } else if (item.startsWith("unmark")) {
                     int number = Integer.parseInt(item.substring(7));
-                    if (number > size || number <= 0) {
+                    if (number > listTask.size() || number <= 0) {
                         throw new NguyenException("Out of range in list of task");
                     }
-                    listTask[number].unMark();
+                    listTask.get(number - 1).unMark();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(listTask[number]);
+                    System.out.println(listTask.get(number - 1));
                 } else {
                     if (item.startsWith("todo")) {
                         if (item.length() == 4) {
                             throw new NguyenException("Empty task is valid task");
                         }
                         String task = item.substring(4);
-                        size++;
-                        listTask[size] = new Todo(task);
+                        listTask.add(new Todo(task));
                     } else if (item.startsWith("deadline")) {
                         int indexBy = item.indexOf("/by");
                         if (item.length() == 8) {
@@ -54,8 +64,7 @@ public class Nguyen {
                         }
                         String task = item.substring(8, indexBy);
                         String by = item.substring(indexBy + 3);
-                        size++;
-                        listTask[size] = new Deadline(task, by);
+                        listTask.add(new Deadline(task, by));
                     } else if (item.startsWith("event")) {
                         int indexFrom = item.indexOf("/from");
                         int indexTo = item.indexOf("/to");
@@ -74,14 +83,13 @@ public class Nguyen {
                         String task = item.substring(5, indexFrom);
                         String from = item.substring(indexFrom + 5, indexTo);
                         String to = item.substring(indexTo + 3);
-                        size++;
-                        listTask[size] = new Event(task, from, to);
+                        listTask.add(new Event(task, from, to));
                     } else {
                         throw new NguyenException("Invalid operation");
                     }
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(listTask[size]);
-                    System.out.println("Now you have " + size + " tasks in the list.");
+                    System.out.println(listTask.get(listTask.size() - 1));
+                    System.out.println("Now you have " + listTask.size() + " tasks in the list.");
                 }
             } catch(NguyenException e) {
                 System.out.println("Have NguyenException: " + e.getMessage());
