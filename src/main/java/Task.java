64677edfb6625e13764424f5e87997a193e60e14
TrKNguyen <nguyenusaco@gmail.com>
2025-01-30 21:32:12 +0800

    Add Level 3. Mark as Done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,24 @@
 public class Task {
+    protected String desciption;
+    protected boolean isDone;
+    public Task(String desciption) {
+        this.desciption = desciption;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unMark() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + desciption;
+    }
 }
--- src/main/java/TristanNguyen.java
+++ src/main/java/TristanNguyen.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-
 public class TristanNguyen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] listItem = new String[101];
+        Task[] listTask = new Task[101];
         int size = 0;
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm TristanNguyen");
@@ -13,19 +12,31 @@ public class TristanNguyen {
             String item = scanner.nextLine();
             if (item.equals("bye")) {
                 break;
-            } else if (item.equals("list")) {
-                System.out.println("____________________________________________________________");
-                for (int i = 1; i <= size; i++) {
-                    System.out.println(i + ". " + listItem[i]);
-                }
-                System.out.println("____________________________________________________________");
-            } else {
-                size++;
-                listItem[size] = item;
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + item);
-                System.out.println("____________________________________________________________");
             }
+            System.out.println("____________________________________________________________");
+            if (item.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= size; i++) {
+                    System.out.println(i + "." + listTask[i]);
+                }
+            } else if (item.startsWith("mark")) {
+                int number = Integer.parseInt(item.substring(5));
+                // System.out.println(number);
+                listTask[number].mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(listTask[number]);
+            } else if (item.startsWith("unmark")) {
+                int number = Integer.parseInt(item.substring(7));
+                listTask[number].unMark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(listTask[number]);
+            }
+            else {
+                size++;
+                listTask[size] = new Task(item);
+                System.out.println("added: " + item);
+            }
+            System.out.println("____________________________________________________________");
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
